7c3a681b74475dbb433399ee95d149686281507d
Alex Waller <alex@artofalexwaller.com>
2022-05-19 19:49:03 -0400

    setting static folder

--- index.js
+++ index.js
@@ -5,9 +5,8 @@ const path = require("path");
 
 const app = express();
 
-app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "public", "index.html"));
-});
+// set static folder
+app.use(express.static(path.join(__dirname, "public")));
 
 const PORT = process.env.PORT || 3006;
 
