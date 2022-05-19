f1c7edb26da0b340b771a6c0c18be5c8b1e6b240
Alex Waller <alex@artofalexwaller.com>
2022-05-19 19:37:10 -0400

    installing nodemon script and handling home get request

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ const express = require("express");
 
 const app = express();
 
+app.get("/", (req, res) => {
+  res.send("<h1>Hello Oa</h1>");
+});
+
 const PORT = process.env.PORT || 3006;
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
