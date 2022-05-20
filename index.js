5c1cc3962b0ecccf47bf811e57ddcd3f6b09b923
Alex Waller <alex@artofalexwaller.com>
2022-05-19 20:04:52 -0400

    comments

--- index.js
+++ index.js
@@ -5,6 +5,33 @@ const path = require("path");
 
 const app = express();
 
+const comments = [
+  {
+    userId: 34,
+    userName: "user 1",
+    text: "had a great time at the Kewaunee Brewing Company"
+  },
+  {
+    userId: 340,
+    userName: "user 2",
+    text: "burgers were amazing"
+  },
+  {
+    userId: 3579,
+    userName: "user 3",
+    text: "beer is top of the line"
+  },
+  {
+    userId: 45867,
+    userName: "user 4",
+    text: "extremely dog friendly"
+  }
+];
+
+app.get("/api/comments", (req, res) => {
+  res.json(comments);
+});
+
 // set static folder
 app.use(express.static(path.join(__dirname, "public")));
 
